c5728a158176a24d1ddf7226086f932914c908da
Zap1999 <45321175+Zap1999@users.noreply.github.com>
2019-05-16 17:27:20 +0300

    author dao added

--- museum-website/src/main/java/com/softserve/academy/museum/db/AuthorDao.java
+++ museum-website/src/main/java/com/softserve/academy/museum/db/AuthorDao.java
@@ -4,33 +4,113 @@ import com.softserve.academy.museum.entities.Author;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.util.List;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class AuthorDao {
 
+    Connection connection = MySQLConnection.getConnection();
+
     public Author getAuthorById(int id) {
-        Connection connection = MySQLConnection.getConnection();
-        PreparedStatement statement = connection.createStatement();
-        String query = "SELECT id, firstname, lastname "
+        String query = "SELECT * "
                 + "FROM author "
                 + "WHERE id=?;";
+        try {
+
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, id);
+            ResultSet res = statement.executeQuery();
+            Author author = new Author();
+
+            res.next();
+            author.setId(Integer.parseInt(res.getNString(0)));
+            author.setFirstname(res.getNString(1));
+            author.setFirstname(res.getNString(2));
+
+            return author;
+
+        } catch (SQLException e) {
+            System.err.println("Cannot execute 'getAuthorById' author dao.");
+            e.printStackTrace();
+            return null;
+        }
+
     }
 
-    public List<Author> getAll() {
-        return authors;
+    public ArrayList<Author> getAll() {
+        String query = "SELECT * FROM author ";
+        try {
+
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet res = statement.executeQuery();
+
+            ArrayList<Author> list = new ArrayList<>();
+            while (res.next()) {
+                Author author = new Author();
+                author.setId(Integer.parseInt(res.getNString(0)));
+                author.setFirstname(res.getNString(1));
+                author.setFirstname(res.getNString(2));
+                list.add(author);
+            }
+
+            return list;
+
+        } catch (SQLException e) {
+            System.err.println("Cannot execute 'getAll' author dao.");
+            e.printStackTrace();
+            return null;
+        }
     }
 
-    public void save(Author o) {
-        authors.add(o);
+    public void save(Author author) {
+        String query = "INSERT INTO author(id, firstname, lastname) "
+                + "VALUES (?, ?, ?) ";
+        try {
+
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, author.getId());
+            statement.setString(2, author.getFirstname());
+            statement.setString(3, author.getLastname());
+            statement.execute();
+
+        } catch (SQLException e) {
+            System.err.println("Cannot execute 'save' author dao.");
+            e.printStackTrace();
+        }
     }
 
-    public void update(Author o, String[] params) {
-        o.setFirstname(params[0]);
-        o.setLastname(params[1]);
+    public void update(int id, Author author) {
+        String query = "UPDATE author "
+                + "SET firstname = ?, lastname = ? "
+                + "WHERE id=?;";
+        try {
+
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, author.getFirstname());
+            statement.setString(2, author.getLastname());
+            statement.setInt(1, id);
+            statement.execute();
+
+        } catch (SQLException e) {
+            System.err.println("Cannot get author dao.");
+            e.printStackTrace();
+        }
     }
 
-    public void delete(Author o) {
-        authors.remove(o);
+    public void delete(Author author) {
+        String query = "DELETE FROM author "
+                + "WHERE id=?;";
+        try {
+
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, author.getId());
+            statement.execute();
+
+        } catch (SQLException e) {
+            System.err.println("Cannot get author dao.");
+            e.printStackTrace();
+        }
     }
 
 }
